718458e8cab06ea284d40f433dfaa1cd24f283d2
wiktorG54 <witus467@gmail.com>
2025-10-22 11:08:35 +0000

    studentmachine send

---
+++ cpp/cwiczenie1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout<<"podaj x"<<endl;
+int x;
+cin >>x;
+cout<<"podano"<<x<<endl;
+if(x >0){
+cout<<"wieksze od 0 \n";
+}else{
+cout<<"mniejsze lub rowne 0 \n";
+}
+	return 0;
+}
