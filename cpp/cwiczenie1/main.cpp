7e063c6679732d296ffebec3f7c30bd38ca4a953
wiktorG54 <witus467@gmail.com>
2025-10-29 11:57:45 +0000

    wyslanie pracy

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 int main(){
-cout<<"podaj x"<<endl;
-int x;
-cin >>x;
-cout<<"podano"<<x<<endl;
-if(x >0){
-cout<<"wieksze od 0 \n";
-}else{
-cout<<"mniejsze lub rowne 0 \n";
+//cout<<"podaj x"<<endl;
+//int x;
+//cin >>x;
+//cout<<"podano"<<x<<endl;
+//if(x >0){
+//cout<<"wieksze od 0 \n";
+//}else{
+//
+//cout<<"mniejsze lub rowne 0 \n";
+for(int i = 1;a <;
 }
 	return 0;
 }
